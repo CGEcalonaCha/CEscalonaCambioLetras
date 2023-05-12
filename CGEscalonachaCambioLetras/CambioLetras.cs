2788004f9c4778eda0b5659bba9d4c20bb5c852c
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-12 10:48:17 -0600

    Add project files.

---
+++ CGEscalonachaCambioLetras/CambioLetras.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEscalonachaCambioLetras
+{
+     class CambioLetras
+    {
+        string y;
+        string x;
+        string resultado;
+        public static string Palindromo()
+        {
+            Console.WriteLine("Hola ingrese una palabra");
+            string palabra = Console.ReadLine();
+
+            palabra = palabra.ToLower();
+            string palindromo = palabra.Reverse().ToString();
+
+            return "";
+        }
+    }
+}
---
+++ CGEscalonachaCambioLetras/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEscalonachaCambioLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string y;
+            string x;
+            string resultado;
+            public static string Palindromo()
+            {
+                Console.WriteLine("Hola ingrese una palabra");
+                string palabra = Console.ReadLine();
+
+                palabra = palabra.ToLower();
+                string palindromo = palabra.Reverse().ToString();
+
+                return "";
+            }
+        }
+    }
+    }
+}
